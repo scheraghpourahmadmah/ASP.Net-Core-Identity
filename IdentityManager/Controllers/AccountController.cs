09347360ac6ed20756fb5b15f276661c9594e584
scheraghpourahmadmah <sholehch71@gmail.com>
2023-03-24 17:11:55 -0400

    Forgot passwrod

--- IdentityManager/Controllers/AccountController.cs
+++ IdentityManager/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using IdentityManager.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
@@ -9,10 +10,12 @@ namespace IdentityManager.Controllers
     {
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly IEmailSender _emailSender;
 
-        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, IEmailSender emailSender)
         {
             _userManager = userManager;
+            _emailSender = emailSender;
             _signInManager = signInManager;
         }
 
@@ -22,7 +25,7 @@ namespace IdentityManager.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Register(string returnurl=null)
+        public async Task<IActionResult> Register(string returnurl = null)
         {
             ViewData["ReturnUrl"] = returnurl;
             RegisterViewModel registerViewModel = new RegisterViewModel();
@@ -35,12 +38,12 @@ namespace IdentityManager.Controllers
         {
             ViewData["ReturnUrl"] = returnurl;
             returnurl = returnurl ?? Url.Content("~/");
-            if (ModelState.IsValid) 
+            if (ModelState.IsValid)
             {
                 var user = new ApplicationUser { UserName = model.Email, Email = model.Email, Name = model.Name };
-                var result = await _userManager.CreateAsync(user,model.Password);
+                var result = await _userManager.CreateAsync(user, model.Password);
 
-                if(result.Succeeded)
+                if (result.Succeeded)
                 {
                     await _signInManager.SignInAsync(user, isPersistent: false);
                     return LocalRedirect(returnurl);
@@ -60,7 +63,7 @@ namespace IdentityManager.Controllers
         }
 
         [HttpGet]
-        public IActionResult Login(string returnurl=null)
+        public IActionResult Login(string returnurl = null)
         {
             ViewData["ReturnUrl"] = returnurl;
             return View();
@@ -68,7 +71,7 @@ namespace IdentityManager.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Login(LoginViewModel model, string returnurl=null)
+        public async Task<IActionResult> Login(LoginViewModel model, string returnurl = null)
         {
             ViewData["ReturnUrl"] = returnurl;
             returnurl = returnurl ?? Url.Content("~/");
@@ -79,7 +82,7 @@ namespace IdentityManager.Controllers
                 {
                     return LocalRedirect(returnurl);
                 }
-                if(result.IsLockedOut)
+                if (result.IsLockedOut)
                 {
                     return View("Lockout");
                 }
@@ -104,15 +107,36 @@ namespace IdentityManager.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> ForgotPassword(ForgotPasswordViewModel model)
         {
-           
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                if (user == null)
+                {
+                    return RedirectToAction("ForgotPasswordConfirmation");
+                }
+
+                var code = await _userManager.GeneratePasswordResetTokenAsync(user);
+                var callbackurl = Url.Action("ResetPassword", "Account", new { userId = user.Id, code = code }, protocol: HttpContext.Request.Scheme);
+
+                await _emailSender.SendEmailAsync(model.Email, "Reset Password - Identity Manager",
+                 "Please reset your password by clicking here: <a href=\"" + callbackurl + "\">link</a>");
+
+                return RedirectToAction("ForgotPasswordConfirmation");
+            }
 
             return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult ForgotPasswordConfirmation()
+        {
+            return View();
         }
 
 
         private void AddErrors(IdentityResult result)
         {
-            foreach(var error in result.Errors)
+            foreach (var error in result.Errors)
             {
                 ModelState.AddModelError(string.Empty, error.Description);
             }
--- IdentityManager/Service/MailJetEmailSender.cs
+++ IdentityManager/Service/MailJetEmailSender.cs
@@ -1,32 +1,76 @@
-﻿using Mailjet.Client.TransactionalEmails;
-using Mailjet.Client;
+﻿using Mailjet.Client;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Threading.Tasks;
 using Mailjet.Client.Resources;
+using Newtonsoft.Json.Linq;
 
 namespace IdentityManager.Service
 {
     public class MailJetEmailSender : IEmailSender
     {
         private readonly IConfiguration _configuration;
-
+        public MailJetOptions _mailJetOptions;
         public MailJetEmailSender(IConfiguration configuration)
         {
             _configuration = configuration;
         }
         public async Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            MailjetClient client = new MailjetClient(_configuration["MailJet:ApiKey"], _configuration["MailJet:SecretKey"]);
+            //MailjetClient client = new MailjetClient(_configuration["MailJet:ApiKey"], _configuration["MailJet:SecretKey"]);
 
-            var mail = new TransactionalEmailBuilder()
-               .WithFrom(new SendContact(_configuration["Email:From"], _configuration["Email:ApplicationName"]))
-               .WithSubject(subject)
-               .WithHtmlPart(htmlMessage)
-               .WithTo(new SendContact(email))
-               .Build();
-            await client.SendTransactionalEmailAsync(mail);
+            //var mail = new TransactionalEmailBuilder()
+            //   .WithFrom(new SendContact(_configuration["Email:From"], _configuration["Email:ApplicationName"]))
+            //   .WithSubject(subject)
+            //   .WithHtmlPart(htmlMessage)
+            //   .WithTo(new SendContact(email))
+            //   .Build();
+            //var r = await client.SendTransactionalEmailAsync(mail);
+
+
+
+            _mailJetOptions = _configuration.GetSection("MailJet").Get<MailJetOptions>();
+
+            MailjetClient client = new MailjetClient(_mailJetOptions.ApiKey, _mailJetOptions.SecretKey)
+            {
+                Version = ApiVersion.V3_1,
+            };
+            MailjetRequest request = new MailjetRequest
+            {
+                Resource = Send.Resource,
+            }
+             .Property(Send.Messages, new JArray {
+             new JObject {
+      {
+       "From",
+       new JObject {
+        {"Email", _configuration["Email:From"]},
+        {"Name", "Benspark"}
+       }
+      }, {
+       "To",
+       new JArray {
+        new JObject {
+         {
+          "Email",
+          email
+         }, {
+          "Name",
+          "Ben"
+         }
+        }
+       }
+      }, {
+       "Subject",
+      subject
+      },  {
+       "HTMLPart",
+       htmlMessage
+      },
+     }
+             });
+           var r = await client.PostAsync(request);
         }
     }
 }
